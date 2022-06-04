933f3423414cd65d2769c0dbf99316acc56a4b88
Michala Pedersen <Mich2d48@edu.zealand.dk>
2022-06-04 15:48:31 +0200

    burgerbar, js
    Burger bar er nu funktionel, mangler noget css. alle sider har nu den samme nav.bar. javascript er added til burgerbaren.

---
+++ kodeMappeKlar/main.js
@@ -0,0 +1,11 @@
+var lagkage = document.getElementById("navigation");
+
+
+function lagkageFunktion() {
+    if (lagkage.style.display === "block") {
+      lagkage.style.display = "none";
+    } else {
+        lagkage.style.display = "block";
+    }
+  }
+
