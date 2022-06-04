f4b5069d2f7b323fcb02bbd7403006af5f2285af
Alina <alina.sevchenko@gmail.com>
2022-06-04 20:58:12 +0200

    har tilføjet billedgalleri i html, css og javascript

--- kodeMappeKlar/main.js
+++ kodeMappeKlar/main.js
@@ -9,3 +9,34 @@ function lagkageFunktion() {
     }
   }
 
+/* js kode på kager side starter https://www.w3schools.com/howto/howto_js_slideshow.asp  4.juni.2022 */ 
+
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+/*  <!-- js kode på kager side slutter https://www.w3schools.com/howto/howto_js_slideshow.asp  4.juni.2022 -->*/
