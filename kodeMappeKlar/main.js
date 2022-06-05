d28bf5b5918ee71c3de70977acc1404784c5f838
Alina <alina.sevchenko@gmail.com>
2022-06-05 21:14:08 +0200

    har tilføjet et galleri til kagesiden

--- kodeMappeKlar/main.js
+++ kodeMappeKlar/main.js
@@ -29,35 +29,25 @@ function lagkageFunktion() {
     }
   }
  
+/* js kode kode starter https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_multiple 5.juni.2022 */ 
 
-/* js kode på kager side starter https://www.w3schools.com/howto/howto_js_slideshow.asp  4.juni.2022 */ 
+let slideIndex = [1,1];
+let slideId = ["kagetapas", "festkager"]
+showSlides(1, 0);
+showSlides(1, 1);
 
-  let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides(n, no) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+  x[slideIndex[no]-1].style.display = "block";  
 }
-/*  <!-- js kode på kager side slutter https://www.w3schools.com/howto/howto_js_slideshow.asp  4.juni.2022 -->*/
+/*  <!-- js kode på kager side slutter  https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_multiple 5.juni.2022 -->*/
